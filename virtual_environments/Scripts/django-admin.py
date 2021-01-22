1f0ba96cef77d843a2e79d7c4850af6262f475c3
Sal Nasir <themoose009@gmail.com>
2021-01-22 18:37:11 -0500

    venv is now conda based

---
+++ virtual_environments/Scripts/django-admin.py
@@ -0,0 +1,21 @@
+#!C:/Users/salma/my_files/covid/-2021-EGHI-GT-HACK-COVID-19/virtual_environments/bin/python
+# When the django-admin.py deprecation ends, remove this script.
+import warnings
+
+from django.core import management
+
+try:
+    from django.utils.deprecation import RemovedInDjango40Warning
+except ImportError:
+    raise ImportError(
+        'django-admin.py was deprecated in Django 3.1 and removed in Django '
+        '4.0. Please manually remove this script from your virtual environment '
+        'and use django-admin instead.'
+    )
+
+if __name__ == "__main__":
+    warnings.warn(
+        'django-admin.py is deprecated in favor of django-admin.',
+        RemovedInDjango40Warning,
+    )
+    management.execute_from_command_line()
